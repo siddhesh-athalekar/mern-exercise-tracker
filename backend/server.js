fedcf55aa2087b47502ad052d0aa338ecb040ade
siddhesh-athalekar <siddhesh85@gmail.com>
2019-07-31 17:42:59 +0530

    Added routes + implemented basic APIs

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,22 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+// Pick server url from env variable
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true})
 
+// Connect to mongodb database
 const connection = mongoose.connection
 connection.once('open', () => {
     console.log('MongoDB database connection established successfully!')
 })
+
+// Routers
+const exerciseRouter = require('./routes/exercises')
+const userRouter = require('./routes/users')
+
+app.use('/exercises', exerciseRouter)
+app.use('/users', userRouter)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
