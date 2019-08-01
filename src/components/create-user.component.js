b07461029d422490e9b7e66c8fea3a9d2615599a
siddhesh-athalekar <siddhesh85@gmail.com>
2019-08-01 15:53:50 +0530

    Implemented Create User component with dummy data

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,54 @@
 import React from "react"
 
 export default class CreateUser extends React.Component{
+    constructor(props){
+        super(props)
+
+        this.onChangeUsername = this.onChangeUsername.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
+
+        this.state = {
+            username: ''            
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault()
+
+        const user = {
+            username: this.state.username            
+        }
+
+        console.log('created user = ', user)
+        this.setState({
+            username: ''
+        })
+    }
+
     render(){
         return(
             <div>
-                You are in Create User component page
+                <form onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label>Username: </label>
+                        <input 
+                            type='text'
+                            required
+                            className='form-control'
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <input type='submit' value='Create User' className='btn btn-primary' />
+                    </div>
+                </form>
             </div>
         )
     }
