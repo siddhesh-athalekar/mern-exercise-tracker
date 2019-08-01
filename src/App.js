aea5a11a6b7cc3a5c6d54fc96826d15cb0db3255
siddhesh-athalekar <siddhesh85@gmail.com>
2019-08-01 14:10:37 +0530

    Setup routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import 'boostrap/dist/css/boostrap.min.css'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="container">
-      Exercise Tracker
-    </div>
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExerciseList} />
+      <Route path="/edit/:id" exact component={EditExerciseList} />
+      <Route path="/create" exact component={CreateExercise} />
+      <Route path="/user" exact component={CreateUser} />
+    </Router>    
   );
 }
 
